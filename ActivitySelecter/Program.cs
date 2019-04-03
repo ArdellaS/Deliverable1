4e9ac8aa1d3c671dedb2bf9dcdd72fa66c417a78
ardella <ardella.shelton@gmail.com>
2019-04-03 16:16:20 -0400

    first commit for unit 1 lab

---
+++ ActivitySelecter/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+
+namespace Deliverable2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int PickActivity;
+            int PeopleNumber;
+            string SelectedActivity;
+            string SuggestedActivity;
+            string TravelMethod;
+
+
+            Console.WriteLine("Happy Friday! What would you like to do this weekend? Here are some options. Please pick a number");
+            Console.WriteLine("1.) Action");
+            Console.WriteLine("2.) Chilling");
+            Console.WriteLine("3.) Danger");
+            Console.WriteLine("4.) Good Food");
+
+
+            PickActivity = Convert.ToInt32(Console.ReadLine());
+
+            if (PickActivity == 1)
+            {
+                SelectedActivity = "action";
+                SuggestedActivity = "Go-Kart Racing";
+            }
+            else if (PickActivity == 2)
+            {
+                SelectedActivity = "chilling";
+                SuggestedActivity = "hiking";
+            }
+            else if (PickActivity == 3)
+            {
+                SelectedActivity = "danger";
+                SuggestedActivity = "skydiving";
+            }
+            else
+            {
+                SelectedActivity = "good food";
+                SuggestedActivity = "to IHOP";
+            }
+
+
+            Console.WriteLine("How many people are you bringing with you?");
+
+            PeopleNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (PeopleNumber == 0)
+            {
+                TravelMethod = "sneakers.";
+            }
+
+            else if (PeopleNumber <= 4)
+            {
+                TravelMethod = "a sedan.";
+            }
+
+            else if (PeopleNumber <= 10)
+            {
+                TravelMethod = "a Volkswagen bus.";
+            }
+
+            else
+            {
+                TravelMethod = "an airplane.";
+            }
+
+            Console.WriteLine("If you to try " + SelectedActivity + " this weekend, then you should go " + SuggestedActivity + " and you should travel in " + TravelMethod);
+
+            Console.WriteLine("Have a good weekend!");
+            Console.Read();
+
+        }
+    }
+}
---
+++ Lab1/CurrencyConverter.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+
+namespace Lab1
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double FirstAmount;
+            double SecondAmount;
+            double ThirdAmount;
+            double Average;
+            double Smallest;
+            double Largest;
+            double Total;
+
+            Console.WriteLine("Enter a dollar amount:");
+            FirstAmount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second dollar amount:");
+            SecondAmount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the third dollar amount:");
+            ThirdAmount = Convert.ToDouble(Console.ReadLine());
+
+            Average = (FirstAmount + SecondAmount + ThirdAmount) / 3;
+
+            Console.WriteLine("The average amount is: $" + Average);
+
+            if (FirstAmount < SecondAmount && FirstAmount < ThirdAmount)
+            {
+                Smallest = FirstAmount;
+            }
+            else if (SecondAmount < FirstAmount && SecondAmount < ThirdAmount)
+            {
+                Smallest = SecondAmount;
+            }
+            else
+            {
+                Smallest = ThirdAmount;
+            }
+
+            Console.WriteLine("The smallest amount is: $" + Smallest);
+
+
+            if (FirstAmount > SecondAmount && FirstAmount > ThirdAmount)
+            {
+                Largest = FirstAmount;
+            }
+            else if (SecondAmount > FirstAmount && SecondAmount > ThirdAmount)
+            {
+                Largest = SecondAmount;
+            }
+            else
+            {
+                Largest = ThirdAmount;
+            }
+            Console.WriteLine("The largest amount is: $" + Largest);
+
+            Total = FirstAmount + SecondAmount + ThirdAmount;
+
+
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("The amount of dollars in USD is $" + Total);
+            Console.WriteLine("The amount of dollars in Swedish Krona is " + Total * 9.26
+                 + "kr");
+            Console.WriteLine("The amount of dollars in Japanese Yenn is ¥" + Total * 111.48);
+            Console.WriteLine("The amount of dollars in Thai Baht is ฿" + Total * 31.73);
+            Console.Read();
+        }
+    }
+}
+
